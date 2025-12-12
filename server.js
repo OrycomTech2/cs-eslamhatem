39a106840cc90600fedbb9c8d860970e0ad165ef
CyberSecurity-Fue <socialmedia@orycom.com>
2025-12-12 23:48:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const ChatRoom = require('./models/ChatRoom');
 const searchRoutes = require("./routes/searchRoutes");
 
 // Configuration
-const PORT = 8080;
+const PORT = 3000;
 const FRONTEND_ORIGIN = process.env.CORS_ORIGIN || 'https://www.cs-islamhatem.com';
 const MONGO_URI = process.env.MONGO_URI || '*';
 
