68130900a2bb4f870eb8813c5f3b70849ddab91c
CyberSecurity-Fue <socialmedia@orycom.com>
2025-12-13 01:22:13 +0200

    Update r2Client.js

--- services/r2Client.js
+++ services/r2Client.js
@@ -5,8 +5,8 @@ const r2Client = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: process.env.R2_ACCESS_KEY,
-    secretAccessKey: process.env.R2_SECRET_KEY,
+    accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
   },
 });
 
